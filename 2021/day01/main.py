aaec9c79c5162b5ed1e83a91cf3bbbe2951c898e
Massimo Meert <massimomeert@protonmail.com>
2021-12-01 09:39:11 +0100

    2021day01

---
+++ 2021/day01/main.py
@@ -0,0 +1,15 @@
+DATA = [int(x) for x in open("2021/day01/input.txt").readlines()]
+
+
+def part_one(data=DATA):
+    return sum(1 if data[x] > data[x - 1] else 0 for x in range(1, len(data)))
+
+
+def part_two():
+    return part_one(
+        [DATA[x] + DATA[x - 1] + DATA[x - 2] for x in range(len(DATA)) if x >= 2]
+    )
+
+
+print(part_one())
+print(part_two())
