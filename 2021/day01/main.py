15b37af3f457c8fec1a26a03b2d895ab21f6b3a1
Massimo Meert <massimomeert@protonmail.com>
2021-12-01 10:37:40 +0100

    refractor
    thanks to u/AP2008 and u/Steinrikur
    
    - https://www.reddit.com/r/adventofcode/comments/r66vow/comment/hmryrtg/?utm_source=share&utm_medium=web2x&context=3
    
    - https://www.reddit.com/r/adventofcode/comments/r66vow/comment/hmrzmbg/?utm_source=share&utm_medium=web2x&context=3

--- 2021/day01/main.py
+++ 2021/day01/main.py
@@ -3,12 +3,12 @@ with open("2021/day01/input.txt") as f:
 
 
 def part_one(data=DATA):
-    return sum(1 if data[x] > data[x - 1] else 0 for x in range(1, len(data)))
+    return sum(data[x] > data[x - 1] for x in range(1, len(data)))
 
 
 def part_two():
     return part_one(
-        [DATA[x] + DATA[x - 1] + DATA[x - 2] for x in range(len(DATA)) if x >= 2]
+        [DATA[x] + DATA[x - 1] + DATA[x - 2] for x in range(2, len(DATA))]
     )
 
 
