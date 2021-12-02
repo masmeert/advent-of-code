eef752b5bf70d72cc3e7dc52558f690cb90c4af0
Massimo Meert <massimomeert@protonmail.com>
2021-12-02 17:03:55 +0100

    cleaned day01 pas

--- 2021/day01/main.pas
+++ 2021/day01/main.pas
@@ -1,21 +1,22 @@
-PROGRAM DAY01;
+program day01;
 
-VAR
-    sum, x: INTEGER;
-    data: ARRAY[0..9] OF INTEGER = (199,200,208,210,200,207,240,269,260,263);
+var
+    sum, x: integer;
+    (* test data cause icb parsing the real one*)
+    data: array[0..9] if integer = (199,200,208,210,200,207,240,269,260,263);
 
-FUNCTION Increases(step: INTEGER) : INTEGER;
-BEGIN
+function Increases(step: integer) : integer;
+begin
     sum := 0;
-    FOR x := step TO Length(data) DO
-    BEGIN
-      IF data[x] > data[x-step] THEN 
-        sum := sum + 1;
-    END;
+    for x := step to Length(data) do
+    begin
+      if data[x] > data[x-step] then 
+        inc(sum);
+    end;
     Increases := sum
-END;
+end;
 
-BEGIN
+begin
     writeln (Increases(1));
     writeln (Increases(3))
-END.
+end.
