fc2bd4657ee6be48542d66d87ad20ddcb8dbb27b
Massimo Meert <massimomeert@protonmail.com>
2021-12-02 16:57:38 +0100

    added refractored day02 in refractor.py

--- 2021/refractor.py
+++ 2021/refractor.py
@@ -1,10 +1,24 @@
-from typing import List
+from typing import List, Tuple
 
 
-def count_increases(data: List[int], step: int) -> int:
+def sonar_sweep(data: List[int], step: int) -> int:
     """
     DAY01 part 1 & 2
     """
     if step < 0:
         return -1
     return sum(data[n] > data[n - step] for n in range(step, len(data)))
+
+
+def dive(data: List[Tuple[str, int]], part: int) -> int:
+    """
+    DAY02 part 1 & 2
+    """
+    fwd = depth = aim = 0
+    for x in data:
+        if x[0] == "forward":
+            fwd += x[1]
+            depth += x[1] * aim
+        else:
+            aim += -x[1] if x[0] == "up" else x[1]
+    return fwd * (depth if part == 2 else aim)
