040eb9ab371888e10e476ab8f2428bc426d35741
Massimo Meert <massimomeert@protonmail.com>
2021-12-02 09:04:13 +0100

    refractor

--- 2021/day02/main.py
+++ 2021/day02/main.py
@@ -1,24 +1,24 @@
 from operator import mul
 
 with open("2021/day02/input.txt") as f:
-    DATA = [x.strip().split(" ") for x in f.readlines()]
+    splitLine = lambda x: [x[: x.index(" ")], int(x[x.index(" ") + 1 :])]
+    DATA = [splitLine(x.strip()) for x in f.readlines()]
 
 
 def part_one():
     return mul(
         sum(
-            int(x[1]) if x[0] == "down" else -int(x[1])
+            x[1] if x[0] == "down" else -x[1]
             for x in DATA
             if x[0] in ("up, down")
         ),
-        sum(int(x[1]) for x in DATA if x[0] == "forward"),
+        sum(x[1] for x in DATA if x[0] == "forward"),
     )
 
 
 def part_two():
     aim = depth = horizontal = 0
     for x in DATA:
-        x[1] = int(x[1])
         if x[0] == "up":
             aim -= x[1]
         elif x[0] == "down":
