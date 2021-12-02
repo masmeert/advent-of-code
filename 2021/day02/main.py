4f7e6af1699fe5f608d7fa1e1c8be4a35a08973c
Massimo Meert <massimomeert@protonmail.com>
2021-12-02 11:16:19 +0100

    refractor

--- 2021/day02/main.py
+++ 2021/day02/main.py
@@ -14,16 +14,13 @@ def part_one():
 def part_two():
     aim = depth = horizontal = 0
     for x in DATA:
-        if x[0] == "up":
-            aim -= x[1]
-        elif x[0] == "down":
-            aim += x[1]
-        else:
+        if x[0] == "forward":
             horizontal += x[1]
             depth += x[1] * aim
+        else:
+            aim += -x[1] if x[0] == "up" else x[1]
     return depth * horizontal
 
 
-print(DATA)
 print(part_one())
 print(part_two())
