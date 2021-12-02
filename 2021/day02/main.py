0d92de71c7480da882bf57c98f59e6a61c90ab41
Massimo Meert <massimomeert@protonmail.com>
2021-12-02 06:41:20 +0100

    day02

--- 2021/day02/main.py
+++ 2021/day02/main.py
@@ -1,10 +1,36 @@
+from operator import mul
+
 with open("2021/day02/input.txt") as f:
-    DATA = [int(x) for x in f.readlines()]
+    DATA = [x.replace("\n", "") for x in f.readlines()]
 
 
 def part_one():
-    return -1
+    return mul(
+        sum(
+            int(x[x.index(" ") + 1 :])
+            if x[: x.index(" ")] == "down"
+            else -int(x[x.index(" ") + 1 :])
+            for x in DATA
+            if x[: x.index(" ")] in ("up, down")
+        ),
+        sum(int(x[x.index(" ") + 1 :]) for x in DATA if x[: x.index(" ")] == "forward"),
+    )
 
 
 def part_two():
-    return -1
+    aim = depth = horizontal = 0
+    for x in DATA:
+        x = x.split(" ")
+        x[1] = int(x[1])
+        if x[0] == "up":
+            aim -= x[1]
+        elif x[0] == "down":
+            aim += x[1]
+        else:
+            horizontal += x[1]
+            depth += x[1] * aim
+    return depth * horizontal
+
+
+print(part_one())
+print(part_two())
