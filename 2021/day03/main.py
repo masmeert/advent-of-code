b1597212da1d409d04ce172f94f797ac4a3c6fb4
Massimo Meert <massimomeert@protonmail.com>
2021-12-03 07:11:55 +0100

    day03

--- 2021/day03/main.py
+++ 2021/day03/main.py
@@ -1,10 +1,38 @@
+import math
+
 with open("2021/day03/input.txt") as f:
-    DATA = [int(x) for x in f.readlines()]
+    DATA = [x.strip() for x in f.readlines()]
 
 
 def part_one():
-    return -1
+    gamma = epsilon = ""
+    for x in range(len(DATA[0])):
+        counts = [0, 0]
+        for bit in DATA:
+            counts[int(bit[x])] += 1
+        gamma += str(counts.index(max(counts)))
+        epsilon += str(counts.index(min(counts)))
+    return int(gamma, 2) * int(epsilon, 2)
 
 
 def part_two():
-    return -1
+    results = []
+    for boolean in range(2):
+        valid = DATA
+        for i in range(len(DATA[0])):
+            counts = [[], []]
+            for bit in valid:
+                counts[int(bit[i])].append(bit)
+            valid = (
+                counts[1 if boolean else 0]
+                if len(counts[1]) >= len(counts[0])
+                else counts[0 if boolean else 1]
+            )
+            if len(valid) == 1:
+                break
+        results.append(valid[0])
+    return math.prod(int("".join(result), 2) for result in results)
+
+
+print(part_one())
+print(part_two())
