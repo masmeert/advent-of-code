bf7192a68a7137c6c026e7ba7dce0a3700fb34e0
Massimo Meert <massimomeert@protonmail.com>
2021-12-03 22:03:35 +0100

    started 2020 day04

---
+++ 2020/day04.py
@@ -0,0 +1,20 @@
+with open("2020/inputs/day04") as f:
+    DATA = [
+        {sub[0]: sub[1] for sub in line if sub[0] != "cid"}
+        for line in [
+            [(x.split(":")) for x in x.split()] for x in f.read().split("\n\n")
+        ]
+    ]
+
+def part_one():
+    return sum(1 for x in DATA if len(x.keys()) == 7)
+
+def part_two():
+    checkers = {
+            "byr": lambda x: return 1920 <= int(x) <= 2002
+            "iyr": lambda x: return 2010 <= int(x) <= 2020
+    }
+
+
+print(part_one())
+print(part_two())
