33e19930bb2efedfc653c1dbf1e55d4d41a8c9a2
Massimo Meert <massimomeert@protonmail.com>
2021-12-03 12:55:10 +0100

    refractor

--- 2021/day03.py
+++ 2021/day03.py
@@ -3,28 +3,35 @@ from typing import List, Union
 
 with open("2021/inputs/day03") as f:
     DATA = [x.strip() for x in f.readlines()]
+
 N = len(DATA[0])
 
 
 def part_one() -> int:
     gamma = epsilon = ""
+
     for i in range(N):
         c = Counter(bits[i] for bits in DATA)
         gamma += c.most_common()[0][0]
         epsilon += c.most_common()[1][0]
+
     return int(gamma, 2) * int(epsilon, 2)
 
 
 def part_two(o2: bool, i: int = 0, data: List[str] = DATA) -> Union[List[str], int]:
     c = Counter(bits[i] for bits in data)
     common = c.most_common()
+
     if common[0][1] != common[1][1]:
         bit = common[0][0] if o2 else common[1][0]
     else:
         bit = "1" if o2 else "0"
+
     valid = [bits for bits in data if bits[i] == bit]
+
     if len(valid) == 1:
         return int(valid[0], 2)
+
     return part_two(o2, i + 1, valid)
 
 
