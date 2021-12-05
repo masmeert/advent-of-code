0041095b9c04912ae70cdbd7fc7f834e77ab793a
Massimo Meert <massimomeert@protonmail.com>
2021-12-05 13:30:18 +0100

    refractor

--- 2021/day05.py
+++ 2021/day05.py
@@ -6,9 +6,9 @@ with open("2021/inputs/day05.txt") as f:
         for line in f.readlines()
     ] # [(x1, y1),(x2, y2)]
 
-
 def count_points(part2: bool) -> int:
     points = Counter()
+    delta = lambda x: 1 if x else -1
     for line in LINES:
         x1, y1 = line[0]
         x2, y2 = line[1]
@@ -17,8 +17,8 @@ def count_points(part2: bool) -> int:
         elif y1 == y2:
             points.update((x, y1) for x in range(min(x1, x2), max(x1, x2) + 1))
         elif part2 and abs(x2 - x1) == abs(y2 - y1):
-            dx = 1 if x2 > x1 else -1
-            dy = 1 if y2 > y1 else -1
+            dx = delta(x2 > x1)
+            dy = delta(y2 > y1)
             points.update((x1 + i * dx, y1 + i * dy) for i in range(abs(x2 - x1) + 1))
     return sum(count > 1 for count in points.values())
 
