007c16b555e4ce3a7e8573e0039aaf09f852016a
Massimo Meert <massimomeert@protonmail.com>
2021-12-05 13:14:22 +0100

    day05

---
+++ 2021/day05.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+with open("2021/inputs/day05.txt") as f:
+    LINES = [
+        [tuple(map(int, coords.split(","))) for coords in line.strip().split(" -> ")]
+        for line in f.readlines()
+    ] # [(x1, y1),(x2, y2)]
+
+
+def count_points(part2: bool) -> int:
+    points = Counter()
+    for line in LINES:
+        x1, y1 = line[0]
+        x2, y2 = line[1]
+        if x1 == x2:
+            points.update((x1, y) for y in range(min(y1, y2), max(y1, y2) + 1))
+        elif y1 == y2:
+            points.update((x, y1) for x in range(min(x1, x2), max(x1, x2) + 1))
+        elif part2 and abs(x2 - x1) == abs(y2 - y1):
+            dx = 1 if x2 > x1 else -1
+            dy = 1 if y2 > y1 else -1
+            points.update((x1 + i * dx, y1 + i * dy) for i in range(abs(x2 - x1) + 1))
+    return sum(count > 1 for count in points.values())
+
+
+print(count_points(False))
+print(count_points(True))
