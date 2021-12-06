da7eb00cc71ba5373e35a4cd551dc7a539335c0c
Massimo Meert <massimomeert@protonmail.com>
2021-12-06 16:04:00 +0100

    back to own code

--- 2021/day06.py
+++ 2021/day06.py
@@ -5,10 +5,13 @@ def simulate_fishes(days: int) -> int:
     fishes = Counter(
         int(x) for x in open("2021/inputs/day06.txt").read().strip().split(",")
     )
-    for d in range(days):
-        fishes[(d + 7) % 9] += fishes[d % 9]
+    for _ in range(days):
+        pregnant = fishes[0]
+        for age in range(8):
+            fishes[age] = fishes[age + 1]
+        fishes[8] = pregnant
+        fishes[6] += pregnant
     return sum(fishes.values())
-
 
 print(simulate_fishes(80))
 print(simulate_fishes(256))
