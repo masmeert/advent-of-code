c7986e72746ae5ed3566d3a1d82c622b7c79ca13
Massimo Meert <massimomeert@protonmail.com>
2021-12-07 16:48:52 +0100

    type hinting

--- 2021/day07.py
+++ 2021/day07.py
@@ -4,11 +4,11 @@ with open("2021/inputs/day07.txt") as f:
     DATA = sorted([int(x) for x in f.readline().split(",")])
 
 
-def part_one():
+def part_one() -> int:
     return sum(abs(x - DATA[len(DATA) // 2]) for x in DATA)
 
 
-def part_two():
+def part_two() -> int:
     return sum(gaussian_sum(abs(x - sum(DATA) // len(DATA))) for x in DATA)
 
 
