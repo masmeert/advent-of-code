5850d1217623215ae270cc07f8ada79428b63e72
Massimo Meert <massimomeert@protonmail.com>
2021-12-09 17:04:14 +0100

    day09 p2

--- 2021/day09.py
+++ 2021/day09.py
@@ -1,28 +1,48 @@
-from typing import List, Set
+from typing import List
+from collections import deque
+from math import prod
 
 DATA = [x.strip() for x in open("2021/inputs/day09.txt").readlines()]
 R, C = len(DATA), len(DATA[0])
 DATA = {(k // C, k % C): int(DATA[k // C][k % C]) for k in range(R * C)}
 
 
-def adjacents(coord: List[int]) -> Set[int]:
+def adjacents(coord: List[int]) -> List[int]:
     r, c = coord
     return {
-        DATA.get((r + 1, c), float("inf")),
-        DATA.get((r - 1, c), float("inf")),
-        DATA.get((r, c + 1), float("inf")),
-        DATA.get((r, c - 1), float("inf")),
+        (r + 1, c): DATA.get((r + 1, c), float("inf")),
+        (r - 1, c): DATA.get((r - 1, c), float("inf")),
+        (r, c + 1): DATA.get((r, c + 1), float("inf")),
+        (r, c - 1): DATA.get((r, c - 1), float("inf")),
     }
 
 
 def part_one() -> int:
     return sum(
-        1 + height for coords, height in DATA.items() if height < min(adjacents(coords))
+        1 + height for coords, height in DATA.items() if height < min(adjacents(coords).values())
     )
 
 
 def part_two() -> int:
-    return -1
-
+    sizes = []
+    seen = set()
+    for k in range(R*C):
+        r,c = (k//C, k%C)
+        if (r,c) not in seen and DATA.get((r,c)) != 9:
+            size = 0
+            stack = deque()
+            stack.append((r,c))
+            while stack:
+                r,c = stack.popleft()
+                if (r,c) in seen:
+                    continue
+                seen.add((r,c))
+                size += 1
+                for coords, height in adjacents((r,c)).items():
+                    if height < 9:
+                        stack.append(coords)
+            sizes.append(size)
+    return prod(sorted(sizes)[-3:])
 
 print(part_one())
+print(part_two())
