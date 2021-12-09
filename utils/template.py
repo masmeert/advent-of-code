a138b5e669e8dcbe8a32cf3b978e04d2a5ac52fc
Massimo Meert <massimomeert@protonmail.com>
2021-12-09 12:44:22 +0100

    refractor template

--- utils/template.py
+++ utils/template.py
@@ -5,8 +5,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SESSION = os.getenv("session")
-YEAR, DAY = sys.argv[1:]
-TEMPLATE = f"""with open("{YEAR}/inputs/day{DAY}.txt") as f:
+
+
+def day_to_string(day: int) -> str | int:
+    return f"{day:02d}" if day in {1, 2, 3, 4, 5, 6, 7, 8, 9} else day
+
+
+def get_input(year: int, day: int):
+    res = requests.get(
+        f"https://adventofcode.com/{year}/day/{day}/input", cookies={"session": SESSION}
+    )
+    if not res.ok:
+        raise RuntimeError(
+            f"Request failed, code: {res.status_code}, message: {res.content}"
+        )
+    return res.text[:-1]
+
+
+def create_file(filename: str, content: str):
+    try:
+        f = open(filename, "x")
+    except Exception as e:
+        raise e
+    else:
+        f.write(content)
+
+
+def main() -> None:
+    year, day = sys.argv[1:]
+    template = f"""with open("{year}/inputs/day{day_to_string(day)}.txt") as f:
     DATA = [x.strip() for x in f.readlines()]
 
 
@@ -20,41 +47,10 @@ def part_two() -> int:
 
 print(part_one())
 print(part_two())
-"""
+    """
+    create_file(f"{year}/day{day_to_string(day)}.py", template)
+    create_file(f"{year}/inputs/day{day_to_string(day)}.txt", get_input(year, day))
 
 
-def day_to_string() -> str:
-    if len(str(DAY)) == 1:
-        return "0" + str(DAY)
-    return DAY
-
-
-def get_input():
-    res = requests.get(
-        f"https://adventofcode.com/{YEAR}/day/{DAY}/input", cookies={"session": SESSION}
-    )
-    if not res.ok:
-        raise RuntimeError(
-            f"Request failed, code: {res.status_code}, message: {res.content}"
-        )
-    return res.text[:-1]
-
-
-# code template
-try:
-    f = open(f"{YEAR}/day{day_to_string()}.py", "x")
-except Exception as e:
-    print(e)
-else:
-    f.write(TEMPLATE)
-    f.close()
-
-# input
-try:
-    data = get_input()
-    f = open(f"{YEAR}/inputs/day{day_to_string()}.txt", "x")
-except Exception as e:
-    print(e)
-else:
-    f.write(data)
-    f.close()
+if __name__ == "__main__":
+    main()
