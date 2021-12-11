3852a57123dcbc65e205725709a3839e41930488
Massimo Meert <massimomeert@protonmail.com>
2021-12-11 21:03:56 +0100

    day11p2

--- 2021/day11.py
+++ 2021/day11.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 SQUIDS = [line.strip() for line in open("2021/inputs/day11.txt").readlines()]
 R, C = len(SQUIDS), len(SQUIDS[0])
 SQUIDS = {(k // C, k % C): int(SQUIDS[k // C][k % C]) for k in range(R * C)}
@@ -19,26 +21,21 @@ def adjacents(r: int, c: int):
     )
 
 
-def part_one() -> int:
-    total = 0
-    for _ in range(100):
-        for c in SQUIDS:
-            SQUIDS[c] += 1
-        to_flash = {c for c in SQUIDS if SQUIDS[c] > 9}
-        while to_flash:
-            c = to_flash.pop()
-            SQUIDS[c] = 0
-            total += 1
-            for a in adjacents(*c):
-                SQUIDS[a] += 1
-                if SQUIDS[a] > 9:
-                    to_flash.add(a)
-    return total
-
-
-def part_two() -> int:
-    return -1
-
-
-print(part_one())
-print(part_two())
+flashed = 0
+for i in count():
+    if i == 100:
+        print(f"p1: {flashed}")
+    for c in SQUIDS:
+        SQUIDS[c] += 1
+    to_flash = {c for c in SQUIDS if SQUIDS[c] > 9}
+    while to_flash:
+        c = to_flash.pop()
+        SQUIDS[c] = 0
+        flashed += 1
+        for a in adjacents(*c):
+            SQUIDS[a] += 1
+            if SQUIDS[a] > 9:
+                to_flash.add(a)
+    if sum(SQUIDS.values()) == 0:
+        print(f"p2: {i+1}")
+        break
