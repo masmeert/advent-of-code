eb58cd09cb75e39b3b9b53d7062658853e61dd84
Massimo Meert <massimomeert@protonmail.com>
2021-12-13 14:03:27 +0100

    day13

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,21 +1,36 @@
-DOTS = [
-    [*map(int, x.strip().split(","))]
+import matplotlib.pyplot as plt
+from typing import Set, Tuple
+
+
+DOTS = set(
+    tuple(map(int, x.strip().split(",")))
     for x in open("2021/inputs/day13_1.txt").readlines()
-]
-INSTRUCTIONS = [
-    x.strip().replace("fold along ", "")
+)
+INS = [
+    x.strip().replace("fold along ", "").split("=")
     for x in open("2021/inputs/day13_2.txt").readlines()
 ]
-print(INSTRUCTIONS)
 
 
-def part_one() -> int:
-    return -1
+def fold(p2: bool, grid: Set[Tuple[int]] = DOTS) -> int | Set[Tuple[int]]:
+    for instruction in INS:
+        axis, line = instruction
+        line = int(line)
+        new_grid = set()
+        for x, y in grid:
+            if axis == "x" and x > line:
+                x = line - (x - line)
+            elif axis == "y" and y > line:
+                y = line - (y - line)
+            new_grid.add((x, y))
+        grid = new_grid
+        if not p2:
+            return len(grid)
+    return grid
 
 
-def part_two() -> int:
-    return -1
-
-
-print(part_one())
-print(part_two())
+print(fold(False))
+grid = fold(True)
+xmax, ymax = max(grid)
+for y in range(ymax + 1):
+    print("".join("#" if (x, y) in grid else "." for x in range(xmax + 1)))
