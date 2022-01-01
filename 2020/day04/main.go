75460686346ed0ec27fb7a7a95b2889115211336
Massimo Meert <massimomeert@protonmail.com>
2022-01-01 11:26:23 +0100

    2020d04p2

--- 2020/day04/main.go
+++ 2020/day04/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -22,7 +24,7 @@ func GetInputMap(input [][]string) (ret InputMap) {
 }
 
 func GetKeys(m map[string]string) (keys []string) {
-	for _, k := range m {
+	for k := range m {
 		keys = append(keys, k)
 	}
 	return keys
@@ -37,7 +39,58 @@ func PartOne(inMap InputMap) (sum int) {
 	return sum
 }
 
+func CheckHgt(hgt string) bool {
+	if len(hgt) < 2 {
+		return false
+	}
+	suffix := hgt[len(hgt)-2:]
+	val, err := strconv.Atoi(hgt[:len(hgt)-2])
+	if err != nil {
+		return false
+	}
+	switch suffix {
+	case "cm":
+		if val < 150 || val > 193 {
+			return false
+		}
+	case "in":
+		if val < 59 || val > 76 {
+			return false
+		}
+	default:
+		return false
+	}
+	return true
+}
+
+func CheckFields(b ...bool) bool {
+	for _, c := range b {
+		if c {
+			return true
+		}
+	}
+	return false
+}
+
+func PartTwo(inMap InputMap) (sum int) {
+	for _, m := range inMap {
+		if !CheckFields(
+			m["byr"] > "2002" || m["byr"] < "1920" || len(m["byr"]) != 4,
+			m["iyr"] > "2020" || m["iyr"] < "2010" || len(m["iyr"]) != 4,
+			m["eyr"] > "2030" || m["eyr"] < "2020" || len(m["eyr"]) != 4,
+			!CheckHgt(m["hgt"]),
+			!regexp.MustCompile("^#[0-9a-f]{6}$").Match([]byte(m["hcl"])),
+			!regexp.MustCompile("^(amb|blu|brn|gry|grn|hzl|oth)$").Match([]byte(m["ecl"])),
+			!regexp.MustCompile("^[0-9]{9}$").Match([]byte(m["pid"])),
+		) {
+			sum += 1
+		}
+	}
+	return sum
+}
+
 func main() {
 	inMap := GetInputMap(GetInput())
 	fmt.Println(PartOne(inMap))
+	fmt.Println(PartTwo(inMap))
 }
