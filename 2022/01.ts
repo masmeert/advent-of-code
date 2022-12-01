916ef0ff968e62272ec992c2ebb1e16fe6830bc9
Massimo Meert <pro@masmeert.com>
2022-12-01 08:33:42 +0100

    cleaner ?

--- 2022/01.ts
+++ 2022/01.ts
@@ -1,13 +1,14 @@
 import { sumArray } from "./utils/array.ts";
 
-Deno.readTextFile("./inputs/01.txt").then((data) => {
-  const highest = data
+const highest = await Deno.readTextFile("./inputs/01.txt").then((data) => {
+  return data
     .split("\n\n")
     .map((group) =>
       sumArray(group.split("\n").map((calories) => parseInt(calories)))
     )
     .sort((a, b) => a - b)
     .slice(-3);
-  console.log(highest[0]);
-  console.log(sumArray(highest));
 });
+
+console.log(highest[0]);
+console.log(sumArray(highest));
