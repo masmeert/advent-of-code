b1b616b852cb483de2c98e7aae4c022dc1df8c9d
Massimo Meert <pro@masmeert.com>
2022-12-01 08:45:15 +0100

    add more array utils

--- 2022/01.ts
+++ 2022/01.ts
@@ -1,14 +1,11 @@
-import { sumArray } from "./utils/array.ts";
+import { sum, nums } from "./utils/array.ts";
 
 const highest = await Deno.readTextFile("./inputs/01.txt").then((data) => {
   return data
     .split("\n\n")
-    .map((group) =>
-      sumArray(group.split("\n").map((calories) => parseInt(calories)))
-    )
-    .sort((a, b) => a - b)
-    .slice(-3);
+    .map((group) => sum(nums(group.split("\n"))))
+    .sort((a, b) => a - b);
 });
 
-console.log(highest[0]);
-console.log(sumArray(highest));
+console.log(highest[highest.length - 1]);
+console.log(sum(highest.slice(-3)));
--- 2022/utils/array.ts
+++ 2022/utils/array.ts
@@ -1,3 +1,7 @@
-export function sumArray(array: number[]) {
+export function nums(array: string[]): number[] {
+  return array.map((num) => parseInt(num));
+}
+
+export function sum(array: number[]): number {
   return array.reduce((partial, i) => partial + i, 0);
 }
