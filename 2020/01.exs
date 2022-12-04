63b7821886ba2ae51a1967017f6628e33a5fb1fd
Massimo Meert <pro@masmeert.com>
2022-12-04 13:14:25 +0100

    practice with 2020 problems

--- 2020/01.exs
+++ 2020/01.exs
@@ -3,17 +3,47 @@ defmodule DayOne do
     {:ok, values} = File.read("./inputs/01.txt")
 
     values
-    |> String.split("\n")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 
   def part_one do
     input = read_input()
 
+    input
+    |> Enum.map(fn x ->
+      input
+      |> Enum.find(fn y -> x + y == 2020 end)
+      |> case do
+        nil -> nil
+        y -> x * y
+      end
+    end)
+    |> Enum.reject(&is_nil/1)
+    |> List.first()
+    |> IO.inspect()
   end
 
   def part_two do
     input = read_input()
 
+    input
+    |> Enum.map(fn x ->
+      input
+      |> Enum.map(fn y ->
+        input
+        |> Enum.find(fn z -> x + y + z == 2020 end)
+        |> case do
+          nil -> nil
+          z -> x * y * z
+        end
+      end)
+      |> Enum.reject(&is_nil/1)
+      |> List.first()
+    end)
+    |> Enum.reject(&is_nil/1)
+    |> List.first()
+    |> IO.inspect()
   end
 end
 
