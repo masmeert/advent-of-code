e46ebc665273666bd9e72844510715f1a30ceebe
Massimo Meert <pro@masmeert.com>
2022-12-05 18:10:16 +0100

    add late day05

---
+++ 2022/05.ts
@@ -0,0 +1,60 @@
+function getStacks(): string[][] {
+  return [
+    ["Q", "F", "M", "R", "L", "W", "C", "V"],
+    ["D", "Q", "L"],
+    ["P", "S", "R", "G", "W", "C", "N", "B"],
+    ["L", "C", "D", "H", "B", "Q", "G"],
+    ["V", "G", "L", "F", "Z", "S"],
+    ["D", "G", "N", "P"],
+    ["D", "Z", "P", "V", "F", "C", "W"],
+    ["C", "P", "D", "M", "S"],
+    ["Z", "N", "W", "T", "V", "M", "P", "C"],
+  ];
+}
+
+function getStackTop(stacks: string[][]) {
+  let top = "";
+  for (const stack of stacks) {
+    top += stack[stack.length - 1];
+  }
+  return top;
+}
+
+function parseInstruction(line: string): number[] {
+  return Array.from(line.matchAll(/[0-9]+/g), (m) => parseInt(m[0]));
+}
+
+function move(instructions: string[], stacks: string[][]): void {
+  for (const line of instructions) {
+    const [n, from, to] = parseInstruction(line);
+    for (let i = 0; i < n; i++) {
+      const disk = stacks[from - 1].pop();
+      stacks[to - 1].push(disk!);
+    }
+  }
+}
+
+function move2(instructions: string[], stacks: string[][]): void {
+  for (const line of instructions) {
+    const [n, from, to] = parseInstruction(line);
+    const remove = stacks[from - 1].splice(stacks[from - 1].length - n, n);
+    stacks[to - 1] = stacks[to - 1].concat(remove);
+  }
+}
+
+function part1(instructions: string[], stacks: string[][]): string {
+  move(instructions, stacks);
+  return getStackTop(stacks);
+}
+
+function part2(instructions: string[], stacks: string[][]): string {
+  move2(instructions, stacks);
+  return getStackTop(stacks);
+}
+
+const instructions = await Deno.readTextFile("./inputs/05.txt").then((data) => {
+  return data.split("\n");
+});
+
+console.log(part1(instructions, getStacks()));
+console.log(part2(instructions, getStacks()));
