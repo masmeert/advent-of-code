f61a18e0a8289bf51d84c48eea55a3acdf26e076
Massimo Meert <pro@masmeert.com>
2022-12-10 11:26:57 +0100

    add day10

---
+++ 2022/10.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    program = list(map(str.split, open("inputs/10.txt").read().split("\n")))
+
+    register = [1]
+    for instruction in program:
+        register.append(register[-1])
+        if instruction[0] != "noop":
+            register.append(register[-1] + int(instruction[1]))
+
+    print(sum(register[c - 1] * c for c in range(20, 221, 40)))
+
+    screen = [[" "] * 40 for _ in range(6)]
+    for i, x in enumerate(register):
+        row = i // 40
+        col = i % 40
+        if abs(x - col) <= 1:
+            screen[row][col] = "█"
+
+    print("\n".join("".join(row) for row in screen))
