c5c510cabde3e126baac7bdf24bf022b59e3d9a6
Massimo Meert <pro@masmeert.com>
2022-12-14 10:54:59 +0100

    day14

---
+++ 2022/14.py
@@ -0,0 +1,61 @@
+def get_input():
+    cave = {}
+
+    for line in open("inputs/14.txt").readlines():
+        coords = line.split(" -> ")
+        for f, t in zip(coords[0::], coords[1::]):
+            (fx, fy) = (int(n) for n in f.split(","))
+            (tx, ty) = (int(n) for n in t.split(","))
+
+            for x in range(min(fx, tx), max(fx, tx) + 1):
+                cave[x, fy] = "#"
+
+            for y in range(min(fy, ty), max(fy, ty) + 1):
+                cave[fx, y] = "#"
+
+    return cave
+
+
+def bottom(cave):
+    return max(y for (_, y), val in cave.items() if val == "#")
+
+
+def drop_sand(cave):
+    x = 500
+
+    for y in range(max(y for (_, y), val in cave.items() if val == "#")):
+        if (x, y + 1) not in cave:
+            pass
+        elif (x - 1, y + 1) not in cave:
+            x -= 1
+        elif (x + 1, y + 1) not in cave:
+            x += 1
+        else:
+            cave[(x, y)] = "o"
+            return (x, y) != (500, 0)
+
+    return False
+
+
+def add_floor(cave):
+    bottom_y = bottom(cave)
+    for x in range(-1000, 1000):
+        cave[x, bottom_y + 2] = "#"
+
+
+def simulate(bottom_is_abyss):
+    cave = get_input()
+
+    if not bottom_is_abyss:
+        add_floor(cave)
+
+    while drop_sand(cave):
+        pass
+
+    return cave
+
+
+if __name__ == "__main__":
+    p1 = sum(1 for v in simulate(True).values() if v == "o")
+    p2 = sum(1 for v in simulate(False).values() if v == "o")
+    print(p1, p2)
