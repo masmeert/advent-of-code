cabbbf28ac62bf5e904cc43b9efff482deeee9a4
Massimo Meert <pro@masmeert.com>
2023-12-01 10:45:30 +0100

    fuse two parts into one

--- 2023/01.ts
+++ 2023/01.ts
@@ -10,18 +10,12 @@ const DIGITS_TABLE: { [key: string]: string } = {
   nin: "9",
 };
 
-function recoverCalibrationValue(calibration: string): number {
-  const digits = calibration.match(/\d/g);
-  if (!digits) throw new Error("No digits found");
-
-  return parseInt(digits[0] + digits[digits.length - 1]);
-}
-
-function recoverCalibrationValue2(calibration: string): number {
+function recoverCalibrationValue(calibration: string, p2: boolean): number {
+  const pattern = p2
+    ? /(?:\d|on(?=e)|tw(?=o)|thre(?=e)|four|fiv(?=e)|six|seve(?=n)|eigh(?=t)|nin(?=e))/g
+    : /\d/g;
   const digits = calibration
-    .match(
-      /(?:\d|on(?=e)|tw(?=o)|thre(?=e)|four|fiv(?=e)|six|seve(?=n)|eigh(?=t)|nin(?=e))/g
-    )
+    .match(pattern)
     ?.map((digit) => DIGITS_TABLE[digit] || digit);
   if (!digits) throw new Error("No digits found");
 
@@ -29,7 +23,13 @@ function recoverCalibrationValue2(calibration: string): number {
 }
 
 const input = Deno.readTextFileSync("./inputs/01.txt").split("\n");
-const part1 = input.reduce((acc, i) => acc + recoverCalibrationValue(i), 0);
-const part2 = input.reduce((acc, i) => acc + recoverCalibrationValue2(i), 0);
+const part1 = input.reduce(
+  (acc, i) => acc + recoverCalibrationValue(i, false),
+  0
+);
+const part2 = input.reduce(
+  (acc, i) => acc + recoverCalibrationValue(i, true),
+  0
+);
 
 console.log(`Part 1: ${part1}\nPart 2: ${part2}\n`);
