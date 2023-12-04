69fd3b45f884cb58ee5c36ca133fb80414edabf7
Massimo Meert <pro@masmeert.com>
2023-12-04 08:40:41 +0100

    refactor, remove duplicate code

--- 2023/rust/day04/src/main.rs
+++ 2023/rust/day04/src/main.rs
@@ -17,52 +17,53 @@ fn parse_card_id(card: &str) -> u32 {
         .unwrap()
 }
 
-fn part_one() -> u32 {
-    let mut total: u32 = 0;
-    let file = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    for line in file.split("\n") {
-        if let Some((_, content)) = line.split_once(":") {
+fn process_lines<F>(file_content: &str, mut process_line: F)
+where
+    F: FnMut(&str, &HashSet<u32>, &HashSet<u32>),
+{
+    for line in file_content.split("\n") {
+        if let Some((card, content)) = line.split_once(":") {
             if let Some((winning, hand)) = content.split_once("|") {
                 let winning = parse_nums_set(winning);
                 let hand = parse_nums_set(hand);
-                let intersection_len = winning.intersection(&hand).count() as u32;
-
-                // Part 1: total is factorial of intersection length
-                if intersection_len > 0 {
-                    total += u32::pow(2, intersection_len - 1)
-                } else {
-                    total += 0
-                };
+                process_line(card, &winning, &hand);
             }
         }
     }
+}
+
+fn part_one() -> u32 {
+    let mut total: u32 = 0;
+    let file_content =
+        fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    process_lines(&file_content, |_, winning, hand| {
+        let intersection_len = winning.intersection(hand).count() as u32;
+
+        // Part 1: total is factorial of intersection length
+        if intersection_len > 0 {
+            total += u32::pow(2, intersection_len - 1);
+        }
+    });
     total
 }
 
 fn part_two() -> u32 {
     let mut won: HashMap<u32, u32> = HashMap::new();
-    let file = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let lines = file.split("\n").collect::<Vec<&str>>();
-    let lines_len = lines.len() as u32;
-    for line in lines {
-        if let Some((card, content)) = line.split_once(":") {
-            let card_id = parse_card_id(card);
-            if let Some((winning, hand)) = content.split_once("|") {
-                let winning = parse_nums_set(winning);
-                let hand = parse_nums_set(hand);
-                let intersection_len = winning.intersection(&hand).count() as u32;
+    let file_content =
+        fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    process_lines(&file_content, |card, winning, hand| {
+        let card_id = parse_card_id(card);
+        let intersection_len = winning.intersection(hand).count() as u32;
 
-                // Part 2: for each won add n copies
-                for n in (card_id + 1)..(card_id + 1 + intersection_len) {
-                    let mut copies = *won.get(&n).unwrap_or(&0);
-                    copies += 1;
-                    copies += *won.get(&card_id).unwrap_or(&0);
-                    won.insert(n, copies);
-                }
-            }
+        // Part 2: for each won add n copies
+        for n in (card_id + 1)..(card_id + 1 + intersection_len) {
+            let mut copies = *won.get(&n).unwrap_or(&0);
+            copies += 1;
+            copies += *won.get(&card_id).unwrap_or(&0);
+            won.insert(n, copies);
         }
-    }
-    won.values().sum::<u32>() + lines_len
+    });
+    won.values().sum::<u32>() + file_content.lines().count() as u32
 }
 
 fn main() {
