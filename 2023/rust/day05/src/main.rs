c6f61e639e2eee3c2943838eafe4065b3ab60888
Massimo Meert <pro@masmeert.com>
2023-12-05 10:04:55 +0100

    Add day05

---
+++ 2023/rust/day05/src/main.rs
@@ -0,0 +1,119 @@
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use std::fs;
+
+#[derive(Debug)]
+struct Converter {
+    destination: usize,
+    source: usize,
+    range_length: usize,
+}
+
+impl Converter {
+    fn apply(&self, input: usize) -> Option<usize> {
+        if input >= self.source && input < self.source + self.range_length {
+            Some(self.destination + (input - self.source))
+        } else {
+            None
+        }
+    }
+}
+
+impl From<String> for Converter {
+    fn from(input: String) -> Self {
+        let parts = input
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+
+        Self {
+            destination: parts[0],
+            source: parts[1],
+            range_length: parts[2],
+        }
+    }
+}
+
+struct CategoryMapper {
+    conversions: Vec<Converter>,
+}
+
+impl CategoryMapper {
+    fn convert(&self, input: usize) -> usize {
+        self.conversions
+            .iter()
+            .find_map(|t| t.apply(input))
+            .unwrap_or_else(|| input)
+    }
+}
+
+impl From<String> for CategoryMapper {
+    fn from(input: String) -> Self {
+        let lines = input.trim().lines().skip(1);
+        Self {
+            conversions: lines.map(|line| line.trim().to_string().into()).collect(),
+        }
+    }
+}
+
+fn parse_seeds(line: &str) -> Vec<usize> {
+    line.trim()
+        .split(": ")
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect()
+}
+
+fn read_input() -> (Vec<usize>, Vec<CategoryMapper>) {
+    let file = fs::read_to_string("input.txt").expect("Error reading input.txt");
+    let input = file.trim().split("\n\n").collect::<Vec<&str>>();
+    let seeds = parse_seeds(input[0]);
+    let maps = input[1..]
+        .iter()
+        .map(|s| CategoryMapper::from(s.to_string()))
+        .collect();
+
+    (seeds, maps)
+}
+
+fn calculate_transformation(seed: usize, maps: &[CategoryMapper]) -> usize {
+    let mut transformed = seed;
+    for map in maps {
+        transformed = map.convert(transformed);
+    }
+    transformed
+}
+
+fn part_one() -> Option<usize> {
+    let (seeds, maps) = read_input();
+    seeds
+        .iter()
+        .map(|&seed| calculate_transformation(seed, &maps))
+        .min()
+}
+
+fn part_two() -> Option<usize> {
+    let (seeds, maps) = read_input();
+    seeds
+        .chunks(2)
+        .map(|chunk| (chunk[0]..chunk[0] + chunk[1]))
+        .flatten()
+        .collect::<Vec<usize>>()
+        .par_iter()
+        .map(|&seed| calculate_transformation(seed, &maps))
+        .min()
+}
+
+// Main function
+fn main() {
+    println!(
+        "Part one: {}",
+        part_one().expect("Failed to calculate part1")
+    );
+    println!(
+        "Part two: {}",
+        part_two().expect("Failed to calculate part2")
+    );
+}
