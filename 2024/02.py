843e3d8da46ff6fef415bf8a29075adbf65a7b24
Massimo Meert <massimo@adimian.com>
2024-12-02 14:39:56 +0100

    improve it a bit

--- 2024/02.py
+++ 2024/02.py
@@ -1,17 +1,15 @@
 from utils import aoc
 
 def is_safe(report: list[int]) -> bool:
-    differences = [report[i+1] - report[i] for i in range(len(report) - 1)]
-    return (all(d > 0 for d in differences) or all(d < 0 for d in differences)) and (all(1 <= abs(d) <= 3 for d in differences))
+    differences = [b - a for a, b in zip(report, report[1:])]
+    return (all(d > 0 for d in differences) or 
+            all(d < 0 for d in differences)) and\
+            all(1 <= abs(d) <= 3 for d in differences)
 
 def is_safe_with_dampener(report: list[int]) -> bool:
     if is_safe(report): 
         return True
-    for i in range(len(report)):
-        modified = report[:i] + report[i+1:]
-        if is_safe(modified): 
-            return True
-    return False
+    return any(is_safe(report[:i] + report[i+1:]) for i in range(len(report)))
 
 if __name__ == "__main__":
     input = aoc.get_input("02")
