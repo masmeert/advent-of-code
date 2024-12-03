14bb38a2c39a2a14fc22b809014b2a19ea795984
Massimo Meert <massimo@adimian.com>
2024-12-03 15:26:18 +0100

    more cleanup

--- 2024/03.py
+++ 2024/03.py
@@ -1,18 +1,18 @@
 import re
 
 
-def parse_memory() -> list[str, str, str]:
+def parse_memory() -> list[str]:
     pattern = r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))"
-    with open(f"inputs/03.txt", "r") as f:
+    with open("inputs/03.txt", "r") as f:
         memory = f.read()
     return re.findall(pattern, memory)
 
 
-def part_one(instructions: list[str, str, str]) -> int:
+def part_one(instructions: list[str]) -> int:
     return sum(int(x) * int(y) for token, x, y in instructions if "mul" in token)
 
 
-def part_two(instructions: list[str, str, str]) -> int:
+def part_two(instructions: list[str]) -> int:
     enabled = True
     total = 0
     for token, x, y in instructions:
