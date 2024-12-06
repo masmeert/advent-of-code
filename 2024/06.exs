98ab684878adac71bc6cb0f91203cacb1ffa393a
Massimo Meert <massimo@adimian.com>
2024-12-06 14:19:53 +0100

    day06

--- 2024/06.exs
+++ 2024/06.exs
@@ -1,82 +1,108 @@
 defmodule DaySix do
-  defp parse_line(line, y) do
-    line
-    |> String.trim()
-    |> String.graphemes()
-    |> Enum.with_index()
-    |> Enum.map(fn {char, x} -> {{x, y}, char} end)
+  def parse_input(file \\ "./inputs/06.txt") do
+    {grid, start} =
+      file
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.with_index()
+      |> Enum.reduce({%{}, nil}, fn {line, y}, {grid, start} ->
+        line
+        |> String.graphemes()
+        |> Enum.with_index()
+        |> Enum.reduce({grid, start}, fn
+          {char, x}, {grid, _} when char in ["^", "v", "<", ">"] ->
+            {Map.put(grid, {x, y}, "."), {{x, y}, direction_to_int(char)}}
+
+          {char, x}, {grid, start} ->
+            {Map.put(grid, {x, y}, char), start}
+        end)
+      end)
+
+    {Map.keys(grid), grid, start}
   end
 
-  defp read_input do
-    "./inputs/06.txt"
-    |> File.stream!()
-    |> Enum.with_index()
-    |> Enum.flat_map(fn {line, y} -> parse_line(line, y) end)
-    |> Enum.into(%{})
-  end
+  def direction_to_int("^"), do: 0
+  def direction_to_int(">"), do: 1
+  def direction_to_int("v"), do: 2
+  def direction_to_int("<"), do: 3
 
-  defp turn_right(:up), do: :right
-  defp turn_right(:right), do: :down
-  defp turn_right(:down), do: :left
-  defp turn_right(:left), do: :up
-
-  defp forward({x, y}, :up), do: {x, y - 1}
-  defp forward({x, y}, :down), do: {x, y + 1}
-  defp forward({x, y}, :left), do: {x - 1, y}
-  defp forward({x, y}, :right), do: {x + 1, y}
-
-  defp is_blocked?(grid, position) do
-    Map.get(grid, position, ".") == "#"
-  end
-
-  defp is_out_of_bounds?(grid, {x, y}) do
-    xs = grid |> Map.keys() |> Enum.map(fn {x, _} -> x end)
-    ys = grid |> Map.keys() |> Enum.map(fn {_, y} -> y end)
-    min_x = Enum.min(xs)
-    max_x = Enum.max(xs)
-    min_y = Enum.min(ys)
-    max_y = Enum.max(ys)
-
-    x < min_x or x > max_x or y < min_y or y > max_y
-  end
-
-  defp find_guard(grid) do
-    {position, char} =
-      grid
-      |> Enum.find(fn {_, c} -> c in ["^", "v", "<", ">"] end)
-
-    direction =
-      case char do
-        "^" -> :up
-        "v" -> :down
-        "<" -> :left
-        ">" -> :right
-      end
-
-    {position, direction}
-  end
-
-  defp simulate_move(grid, position, direction, visited) do
-    next = forward(position, direction)
-
-    cond do
-      is_blocked?(grid, next) ->
-        simulate_move(grid, position, turn_right(direction), visited)
-
-      is_out_of_bounds?(grid, next) ->
-        MapSet.size(visited)
-
-      true ->
-        simulate_move(grid, next, direction, MapSet.put(visited, next))
+  def calculate_next_position({x, y}, direction) do
+    case direction do
+      0 -> {x, y - 1}
+      1 -> {x + 1, y}
+      2 -> {x, y + 1}
+      3 -> {x - 1, y}
     end
   end
 
-  def part_one() do
-    grid = read_input()
-    {position, direction} = find_guard(grid)
-    visited = MapSet.new() |> MapSet.put(position)
-    simulate_move(grid, position, direction, visited)
+  def simulate_movement({grid_points, grid, {start_pos, start_dir}}, obstacle \\ nil) do
+    bounds = {
+      grid_points |> Enum.map(&elem(&1, 0)) |> Enum.min_max(),
+      grid_points |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
+    }
+
+    move(
+      {grid, bounds},
+      {start_pos, start_dir},
+      MapSet.new([start_pos]),
+      MapSet.new([]),
+      obstacle
+    )
+  end
+
+  def move({grid, {{min_x, max_x}, {min_y, max_y}}} = env, {pos, dir}, visited, states, obstacle) do
+    next_pos = calculate_next_position(pos, dir)
+    {x, y} = next_pos
+    current_state = {pos, dir}
+
+    cond do
+      MapSet.member?(states, current_state) ->
+        {:loop_detected, visited}
+
+      x < min_x or x > max_x or y < min_y or y > max_y ->
+        {:out_of_bounds, visited}
+
+      next_pos == obstacle or Map.get(grid, next_pos, "#") == "#" ->
+        next_dir = rem(dir + 1, 4)
+        move(env, {pos, next_dir}, visited, MapSet.put(states, current_state), obstacle)
+
+      true ->
+        move(
+          env,
+          {next_pos, dir},
+          MapSet.put(visited, next_pos),
+          MapSet.put(states, current_state),
+          obstacle
+        )
+    end
+  end
+
+  def part_one(input \\ parse_input()) do
+    input
+    |> simulate_movement()
+    |> elem(1)
+    |> MapSet.size()
+  end
+
+  def part_two(input \\ parse_input()) do
+    {_points, grid, {start_pos, _}} = input
+
+    grid
+    |> Map.filter(fn {pos, value} -> value == "." and pos != start_pos end)
+    |> Map.keys()
+    |> Task.async_stream(
+      fn pos ->
+        case simulate_movement(input, pos) do
+          {:loop_detected, _} -> 1
+          _ -> 0
+        end
+      end,
+      max_concurrency: System.schedulers_online() * 2
+    )
+    |> Enum.map(fn {:ok, result} -> result end)
+    |> Enum.sum()
   end
 end
 
 IO.inspect(DaySix.part_one())
+IO.inspect(DaySix.part_two())
