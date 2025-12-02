27e9a58360a0d7b680c41cc42787546428a9d053
Massimo <massimo@adimian.com>
2025-12-02 09:32:49 +0100

    using regex

--- 2025/src/day02.ts
+++ 2025/src/day02.ts
@@ -8,62 +8,37 @@ function parseRange(s: string): IdRange {
 	return { left, right };
 }
 
+function buildRange(range: IdRange): number[] {
+	return A.range(range.left, range.right);
+}
+
 function parseInput() {
 	return getAocInput({ day: 2, year: 2025 }).pipe(
 		Effect.map((input) => pipe(input.trim().split(","), A.map(parseRange))),
 	);
 }
 
-function isRepeatedPattern(str: string, patternLen: number): boolean {
-	const pattern = str.slice(0, patternLen);
-	const repetitions = str.length / patternLen;
-	return pattern[0] !== "0" && str === pattern.repeat(repetitions);
+function isExactlyTwice(id: number): boolean {
+	return /^(.+)\1$/.test(id.toString());
 }
 
-function isRepeatingSequence(
-	times: (n: number) => boolean,
-): (id: number) => boolean {
-	return (id: number): boolean => {
-		const str = id.toString();
-		const len = str.length;
-
-		return pipe(
-			A.range(1, len - 1),
-			A.filter((patternLen) => len % patternLen === 0),
-			A.filter((patternLen) => times(len / patternLen)),
-			A.some((patternLen) => isRepeatedPattern(str, patternLen)),
-		);
-	};
+function isAtLeastTwice(id: number): boolean {
+	return /^(.+)\1+$/.test(id.toString());
 }
 
 function sumInvalidIds(
 	ranges: IdRange[],
 	predicate: (id: number) => boolean,
 ): number {
-	return pipe(
-		ranges,
-		A.flatMap(({ left, right }) => A.range(left, right)),
-		A.filter(predicate),
-		N.sumAll,
-	);
+	return pipe(ranges, A.flatMap(buildRange), A.filter(predicate), N.sumAll);
 }
 
 function main() {
 	return Effect.gen(function* () {
 		const ranges = yield* parseInput();
 
-		yield* Effect.log(
-			sumInvalidIds(
-				ranges,
-				isRepeatingSequence((n) => n === 2),
-			),
-		);
-		yield* Effect.log(
-			sumInvalidIds(
-				ranges,
-				isRepeatingSequence((n) => n >= 2),
-			),
-		);
+		yield* Effect.log(sumInvalidIds(ranges, isExactlyTwice));
+		yield* Effect.log(sumInvalidIds(ranges, isAtLeastTwice));
 	});
 }
 
